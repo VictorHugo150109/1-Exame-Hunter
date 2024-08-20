50c04a5e61dc6ac9b613069c29226d215805d854
PanPan <00001108621594sp@al.educacao.sp.gov.br>
2024-08-20 13:21:47 -0300

    sketch.js

---
+++ sketch.js
@@ -0,0 +1,36 @@
+let cor;
+let circuloX;
+let circuloY;
+
+function setup() {
+  createCanvas(400, 400);
+  background((100, 0, 0));
+
+  cor = "color(random(0, 255), random(0, 255), random(0,255))";
+  circuloX = [0, 0, 0];
+  circuloY = [random(height), random(height), random(height)];
+}
+function draw() {
+  fill("Purple");
+  textSize(30);
+  text("TA AI MÁRCIO", 69, 50);
+  // console.log(circuloX length);
+  for (let contador in circuloX) {
+    console.log(contador);
+    circle(circuloX[contador], circuloY[contador], 30);
+    circuloX[contador] += random(0, 3);
+    circuloY[contador] += random(-3, 3);
+
+    if (circuloX[contador] >= width) {
+      circuloX[contador] = 0;
+      circuloY[contador] = random(height);
+    }
+  }
+
+  if (mouseIsPressed) {
+    cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
+  }
+}
+
+
+FeitoPorVictor 
